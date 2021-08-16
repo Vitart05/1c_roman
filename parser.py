253509b93ea73c9e22d903f1c43d855f97fd591d
Vitart05 <63416943+Vitart05@users.noreply.github.com>
2021-08-16 23:45:47 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -35,44 +35,55 @@ def count_page():
     soup = BeautifulSoup(file, 'lxml')
     pages = soup.find_all('li', class_='pagination__pages-list-item')
     last_page = pages[-2].text.strip()
+    #print(last_page)
     return int(last_page)
 
 
+def get_page(url):
+    soup = BeautifulSoup(url, 'lxml')
+    prices = soup.find_all('div', class_='catalog-section-items__item')
+    #print(prices)
+    #print(prices)
+    for price in prices:
+        try:
+            product_code = price.find('div', class_='catalog-section-items-item__code').text.strip()
+        except:
+            product_code = ''
+        try:
+            article = price.find('div', class_='catalog-section-items-item__article').text.strip()
+        except:
+            article = ''
+        name1 = price.find('a', class_='catalog-section-items-item__name').text.strip()
+        name1 = name1.replace(',', '.')
+        name2 = price.find('div', class_='catalog-section-items-item__description').text.strip()
+        name2 = name2.replace(',', '.')
+        name = name1 + ' ' + name2
+        try:
+            count1 = price.find('span', class_='catalog-section-items-item__available').text.strip()
+            count = get_price(count1)
+            if count == '':
+                count = '0'
+        except:
+            count = ''
+        try:
+            pric = price.find('div', class_='catalog-section-items-item-price__value').text.strip()
+            price1 = get_price(pric)
+        except:
+            price1 = ''
+        data = {'product_code': product_code, 'article': article, 'name': name, 'count': count, 'price1': price1}
+        #print(data)
+        write_csv(data)
+
+
+
 def main():
-    first_str()
+    #first_str()
     for k in range(1, count_page() + 1):
         file = open('pages/page{}.html'.format(str(k))).read()
-        soup = BeautifulSoup(file, 'lxml')
-        prices = soup.find_all('div', class_='catalog-section-items')
-        for price in prices:
-            try:
-                product_code = price.find('div', class_='catalog-section-items-item__code').text.strip()
-            except:
-                product_code = ''
-            try:
-                article = price.find('div', class_='catalog-section-items-item__article').text.strip()
-            except:
-                article = ''
-            name1 = price.find('a', class_='catalog-section-items-item__name').text.strip()
-            name1 = name1.replace(',', '.')
-            name2 = price.find('div', class_='catalog-section-items-item__description').text.strip()
-            name2 = name2.replace(',', '.')
-            name = name1 + ' ' + name2
-            try:
-                count1 = price.find('span', class_='catalog-section-items-item__available').text.strip()
-                count = get_price(count1)
-                if count == '':
-                    count = '0'
-            except:
-                count = ''
-            try:
-                pric = price.find('div', class_='catalog-section-items-item-price__value').text.strip()
-                price1 = get_price(pric)
-            except:
-                price1 = ''
-            data = {'product_code': product_code, 'article': article, 'name': name, 'count': count, 'price1': price1}
-            write_csv(data)
         print(k)
+        get_page(file)
+
+        #print(k)
 
 
 if __name__ == '__main__':
